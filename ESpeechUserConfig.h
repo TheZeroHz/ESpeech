943ccb44ec30b5e75cf687696c5452a5b23f5b3b
Rakib Hasan <59503211+TheZeroHz@users.noreply.github.com>
2025-09-24 19:40:40 +0600

    Update ESpeechUserConfig.h

--- ESpeechUserConfig.h
+++ ESpeechUserConfig.h
@@ -1,6 +1,6 @@
 #define USE_WAKEWORD
-#define ESPEECH_USE_SPIFFS 1
-#define ESPEECH_USE_FFAT 0
+#define ESPEECH_USE_SPIFFS 0
+#define ESPEECH_USE_FFAT 1
 
 #define VAD_CORE 0
 #define VAD_PRIORITY 1
@@ -8,8 +8,8 @@
 #define VAD_MAX_TIME 10000
 
 const int I2S_WS  = 16;
-const int I2S_SD  = 7;
-const int I2S_SCK = 15;
+const int I2S_SD  = 18;
+const int I2S_SCK = 17;
 
 
 #if ESPEECH_USE_SPIFFS
