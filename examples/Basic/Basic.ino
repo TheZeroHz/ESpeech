123bf9875b9f04ef0243ce74f2df808458a2f4e3
Rakib Hasan <59503211+TheZeroHz@users.noreply.github.com>
2025-09-24 19:41:32 +0600

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,17 +1,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/i2s.h"
-#include "ESpeechUserConfig.h"
 #include <ESpeech.h>
-#define I2S_WS 16    // for esp32_s I2S_WS 25
-#define I2S_SD 7    // for esp32_s I2S_SD 33
-#define I2S_SCK 15    // for esp32_s I2S_SCK 32
-
 ESpeech STT(I2S_NUM_1,I2S_SCK,I2S_WS,I2S_SD);
-
-const char *ssid = "Your SSID";                                // Your SSID
-const char *password = "Your PASS";                       // Your PASS
-#define serverUrl "https://YOUR_SERVER_URL.onrender.com/uploadAudio"  // Change the IP Address according To Your Server's config
+const char *ssid = "BRIC_301/B";                                // Your SSID
+const char *password = "research@301";                       // Your PASS
+#define serverUrl "https://espeechserver-iukg.onrender.com/uploadAudio"  // Change the IP Address according To Your Server's config
 
 void setup() {
   Serial.begin(115200);
